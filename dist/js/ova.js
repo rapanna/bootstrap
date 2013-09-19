ab9f3e9a75d07e0368247d88b29bb2cdf4c424d3
Radomir Panna <radomir.panna@gmail.com>
2013-09-19 10:25:27 +0200

    změněn základní font na Lucida Grande, barva písma, barva odkazů

---
+++ dist/js/ova.js
@@ -0,0 +1,7 @@
+if (typeof Cufon != "undefined") {
+	Cufon.replace('h1');
+	Cufon.replace('h2');
+	Cufon.replace('h2 a', {hover: true});  
+	Cufon.replace('h3');
+	Cufon.replace('h4');  
+	Cufon.replace('a.navbar-brand', {hover: true})}
